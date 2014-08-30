41023fb81a5dc7aeefd8d783fe44c850f576fcc8
Rob Miller <rob@bigfish.co.uk>
2014-08-30 10:50:50 +0100

    Initial script
    - script in bin/ with option parsing
    - library code in lib/rows2cols

--- lib/rows2cols.rb
+++ lib/rows2cols.rb
@@ -1,5 +1,18 @@
 require "rows2cols/version"
 
-module Rows2cols
-  # Your code goes here...
+class Rows2Cols
+  include Enumerable
+
+  def initialize(io: ARGF, columns: 4, separator: " ")
+    @io        = io
+    @columns   = columns
+    @separator = separator
+  end
+
+  def each
+    @io.each_slice(@columns) do |rows|
+      yield rows.map(&:chomp).join(@separator)
+    end
+  end
+  alias :each_row :each
 end
